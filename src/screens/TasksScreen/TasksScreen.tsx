3a56c9ec0dc459c65d79a9de97b6b1b3a60ef390
willianlemes <willianlemes94@gmail.com>
2022-12-01 10:03:14 -0300

    feat: Implementação da TasksScreen

--- src/screens/TasksScreen/TasksScreen.tsx
+++ src/screens/TasksScreen/TasksScreen.tsx
@@ -1,6 +1,50 @@
 import React from "react";
-import { View } from "react-native";
+import { useState } from "react";
+import { StyleSheet, View } from "react-native";
+import { StatusBar } from "expo-status-bar";
+import { getStatusBarHeight } from "react-native-status-bar-height";
+
+import { Tasks, Input } from "../../components";
 
 export function TasksScreen() {
-  return <View />;
+  const [newTask, setNewTask] = useState("");
+  const [tasks, setTasks] = useState<string[]>([]);
+
+  const handleAddTask = (newTask: string) => {
+    const taskFound = tasks.find((t) => t === newTask.trim());
+
+    if (!taskFound) {
+      setTasks((prev) => [...prev, newTask.trim()]);
+    }
+
+    setNewTask("");
+  };
+
+  const handleRemoveTask = (task: string) => {
+    const newTasks = tasks.filter((t) => t !== task);
+    setTasks(newTasks);
+  };
+
+  return (
+    <View style={styles.container}>
+      <StatusBar />
+      <Tasks tasks={tasks} onRemoveTask={handleRemoveTask} />
+      <Input
+        value={newTask}
+        onChangeText={setNewTask}
+        onAddTask={handleAddTask}
+      />
+    </View>
+  );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#063970",
+    paddingTop: getStatusBarHeight(),
+  },
+  tasks: {
+    paddingHorizontal: 10,
+  },
+});
